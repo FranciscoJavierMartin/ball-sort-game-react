9170055b5a83bdd702050fc921f943a802aac2a2
Francisco <github.backstage538@passmail.net>
2025-05-23 19:00:15 +0200

    Home page design

---
+++ src/modules/home/components/logo/logo.tsx
@@ -0,0 +1,9 @@
+import './logo.css';
+
+export default function Logo() {
+  return (
+    <div className='logo'>
+      <span>B</span>all Sort Puzzl<span>e</span>
+    </div>
+  );
+}
--- src/modules/home/pages/home-page.tsx
+++ src/modules/home/pages/home-page.tsx
@@ -1,8 +1,19 @@
+import { Link } from 'react-router';
+import Share from '@/modules/share/components/share/share';
+import Logo from '@/modules/home/components/logo/logo';
+import LevelNumber from '@/modules/home/components/level-number/level-number';
+import { ROUTES } from '@/modules/common/constants/routes';
+import Icon from '@/modules/common/components/icon/icon';
+import './home-page.css';
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>This is the home page.</p>
+    <div className='home-page'>
+      <Logo />
+      <LevelNumber />
+      <Link to={ROUTES.GAME} className='play-button' title='Play'>
+        <Icon type='play' />
+      </Link>
     </div>
   );
 }
